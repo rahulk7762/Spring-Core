838d320e3f8c8da6a98a1c3ed367d2a7d92fa22e
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-02-07 16:24:49 +0530

    Initial commit

---
+++ src/main/java/com/my/spring/Main.java
@@ -0,0 +1,25 @@
+package com.my.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.my.spring.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		Employee bean = ioc.getBean("emp",Employee.class);
+//		bean.setName("Rahul");
+		
+		System.out.println(bean);
+		
+		
+		Employee bean2 = ioc.getBean("emp2",Employee.class);
+		System.out.println(bean2);
+	}
+
+}
---
+++ src/main/java/com/my/spring/model/Employee.java
@@ -0,0 +1,57 @@
+package com.my.spring.model;
+
+public class Employee {
+
+	private int id;
+	private String name,gender;
+	
+	public Employee() {
+		super();
+		System.out.println("Employee()");
+	}
+	
+	public Employee(int id,String name,String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		System.out.println("Employee(3)");
+	}
+	
+
+	public void setId(int id) {
+		System.out.println("Employee.setId()");
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		System.out.println("Employee.setName()");
+		this.name = name;
+	}
+	
+	public void setGender(String gender) {
+		System.out.println("Employee.setGender()");
+		this.gender = gender;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + "]";
+	}
+	
+	
+
+}
